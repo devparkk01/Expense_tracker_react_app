d640056071e79bb4068a6c50f811fb9b8d98aae1
devparkk01 <dev.parkk01@gmail.com>
2021-08-25 21:15:20 +0530

    Added props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,17 +3,17 @@ import './ExpenseItem.css' ;
 
 
 
-function ExpenseItem() {
-    const expenseDate = new Date(2021 , 7 , 25) ;
-    const expenseTitle = "Course Purchase" ;
-    const expensePrice = 21.99  ;
+function ExpenseItem(props) {
+    // const expenseDate = new Date(2021 , 7 , 25) ;
+    // const expenseTitle = "Course Purchase" ;
+    // const expensePrice = 21.99  ;
 
     return (
         <div className = "expense-item">
-            <div>{ expenseDate.toISOString() }</div>
+            <div>{props.date}</div>
             <div className = "expense-item__description">
-                <h2>{ expenseTitle }</h2>
-                <div className = "expense-item__price">${ expensePrice } </div>
+                <h2>{ props.title }</h2>
+                <div className = "expense-item__price">${ props.amount } </div>
             </div>
         </div>
     ); 
