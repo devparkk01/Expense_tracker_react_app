59de9db54a698ba6143b8b7d35a0ba1aea8f1ba8
devparkk01 <dev.parkk01@gmail.com>
2021-08-25 20:26:01 +0530

    Added dynamic data

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -4,12 +4,16 @@ import './ExpenseItem.css' ;
 
 
 function ExpenseItem() {
+    const expenseDate = new Date(2021 , 7 , 25) ;
+    const expenseTitle = "Course Purchase" ;
+    const expensePrice = 21.99  ;
+
     return (
         <div className = "expense-item">
-            <div>25th August 2021 </div>
+            <div>{ expenseDate.toISOString() }</div>
             <div className = "expense-item__description">
-                <h2>Course Purchase</h2>
-                <div className = "expense-item__price">$21.99</div>
+                <h2>{ expenseTitle }</h2>
+                <div className = "expense-item__price">${ expensePrice } </div>
             </div>
         </div>
     ); 
