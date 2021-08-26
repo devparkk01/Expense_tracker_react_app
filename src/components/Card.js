ed0ad5e6697e7c2a6a50eee74726cf2db1597342
devparkk01 <dev.parkk01@gmail.com>
2021-08-26 15:54:11 +0530

    Concept of Composition

---
+++ src/components/Card.js
@@ -0,0 +1,12 @@
+import './Card.css' ; 
+
+
+function Card(props) {
+    console.log(props.className) ;
+    const classes = "card " + props.className ; 
+    return  (
+        <div className = {classes} >{props.children } </div>
+    );
+}
+
+export default Card ; 
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,22 +1,17 @@
-
-import './ExpenseItem.css' ;
-
-
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import Card from './Card' ;
 
 function ExpenseItem(props) {
-    // const expenseDate = new Date(2021 , 7 , 25) ;
-    // const expenseTitle = "Course Purchase" ;
-    // const expensePrice = 21.99  ;
-
-    return (
-        <div className = "expense-item">
-            <div>{props.date}</div>
-            <div className = "expense-item__description">
-                <h2>{ props.title }</h2>
-                <div className = "expense-item__price">${ props.amount } </div>
-            </div>
-        </div>
-    ); 
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date}></ExpenseDate>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount} </div>
+      </div>
+    </Card>
+  );
 }
 
-export default ExpenseItem ;
+export default ExpenseItem;
