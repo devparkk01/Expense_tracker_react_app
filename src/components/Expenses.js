4df29c52f0d4146bcfced5a2fda97d9b7e8d71ae
devparkk01 <dev.parkk01@gmail.com>
2021-09-03 11:42:02 +0530

    Added conditional return

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -3,6 +3,7 @@ import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
 import Card from "./Card";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 
 function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -10,22 +11,21 @@ function Expenses(props) {
     setFilteredYear(selectedYear);
   };
 
+  const filteredExpenses = props.items.filter( (expense) => (
+     expense.date.getFullYear().toString()  === filteredYear)  
+  ) 
+
+
   return (
-    <Card className="expenses">
-      <ExpensesFilter
-        selected={filteredYear}
-        onChangeFilter={changeFilterHandler}
-      />
-      {props.items.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          date={expense.date}
-          amount={expense.amount}
-        ></ExpenseItem>
-      ))}
-      ; ;
-    </Card>
+    <li>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={changeFilterHandler}
+          />
+        <ExpensesList items = {filteredExpenses}></ExpensesList>
+      </Card>
+    </li>
   );
 }
 
---
+++ src/components/ExpensesList.js
@@ -0,0 +1,31 @@
+import React from "react" ; 
+
+import ExpenseItem from "./ExpenseItem";
+import './ExpensesList.css'
+
+const ExpensesList = (props) => {
+
+    if(props.items.length === 0 ) {
+        return <h2 className = "expenses-list__fallback"> No expenses found !</h2>
+    }
+
+    return (
+    <ul className = "expenses-list">
+            {props.items.map( (expense) => (
+                <ExpenseItem
+                    key = {expense.id} 
+                    title = {expense.title}
+                    date = {expense.date}
+                    amount = {expense.amount}
+                >
+                </ExpenseItem>
+            )) }
+        </ul>
+    )
+
+}
+
+
+
+
+export default ExpensesList ; 
